017ad49fadbea101de498c145edd6d3e2be990d9
Matheus Mauricio <121787652+PedrelliMath@users.noreply.github.com>
2024-12-04 20:27:03 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -35,9 +35,10 @@ class SplashPage extends StatefulWidget {
 
 class _SplashPageState extends State<SplashPage> {
   Future<void> _checkLoginStatus() async {
+    // Realiza a busca do token no storage do dispositivo
     final prefs = await SharedPreferences.getInstance();
     final token = prefs.getString('github_token');
-
+    // Caso encontre o token, solicita ao github os dados do user
     if (token != null) {
       final userInfo = await _fetchGitHubUser(token);
       if (userInfo != null) {
@@ -59,6 +60,9 @@ class _SplashPageState extends State<SplashPage> {
     );
   }
 
+  // Função que busca os dados do user na api do github
+  // Recebe como parametro o token do user e retorna o body
+  // da request (os dados do user)
   Future<Map<String, dynamic>?> _fetchGitHubUser(String token) async {
     const url = 'https://api.github.com/user';
     final response = await http.get(
@@ -76,12 +80,15 @@ class _SplashPageState extends State<SplashPage> {
     }
   }
 
+  // Estado inicial da pagina checa o status do login
   @override
   void initState() {
     super.initState();
     _checkLoginStatus();
   }
 
+  // Enquanto o login é verificado um indicador circular
+  // é renderizado
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
@@ -92,6 +99,7 @@ class _SplashPageState extends State<SplashPage> {
   }
 }
 
+// Widget da tela de login
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
@@ -99,6 +107,9 @@ class LoginPage extends StatefulWidget {
   State<LoginPage> createState() => _LoginPageState();
 }
 
+// Cria um objeto do tipo GithubSingIn
+// Este cujo se encarrega de realizar a autenticação
+// Utilizando o github como provider
 class _LoginPageState extends State<LoginPage> {
   final GitHubSignIn gitHubSignIn = GitHubSignIn(
     clientId: 'Ov23limrERaj9llnuFmH',
@@ -106,13 +117,20 @@ class _LoginPageState extends State<LoginPage> {
     redirectUrl: 'https://mobile-n3.firebaseapp.com/__/auth/handler',
   );
 
+  // Função que realiza a autenticação recebe como parametro
+  // o context e retorna não retorna nada pois "troca"
+  // a pagina na navegação, da tela de login para a
+  // welcome page
   Future<void> _signInWithGitHub(BuildContext context) async {
     final result = await gitHubSignIn.signIn(context);
     if (result.status == GitHubSignInResultStatus.ok) {
       final token = result.token;
 
+      // Se o github sign in retornar o token, significa
+      // que a autenticação deu certo
       if (token != null) {
-        // Salva o token para login persistente
+        // Salva o token no storage do dispositivo para persistencia
+        // após fechar o aplicativo
         final prefs = await SharedPreferences.getInstance();
         await prefs.setString('github_token', token);
 
@@ -128,10 +146,18 @@ class _LoginPageState extends State<LoginPage> {
         }
       }
     } else {
+      // Se o githubSignIn não retornar o token, algum erro
+      // ocorreu e cai nessa clausa else, que apenas informa
+      // que houve erro na autenticação
       print("Erro ao fazer login: ${result.errorMessage}");
     }
   }
 
+  // Função que busca as informações do usuário passando o token
+  // como parâmetro e retorna um Map de string:dinamic, isso 
+  // significa com os campos do json não estão mapeados e podem
+  // ser absolutamente qualquer tipo de valor, como um int, double
+  // string, etc
   Future<Map<String, dynamic>?> _fetchGitHubUser(String token) async {
     const url = 'https://api.github.com/user';
     final response = await http.get(
@@ -141,14 +167,18 @@ class _LoginPageState extends State<LoginPage> {
       },
     );
 
+    // Verifica se a requisição foi bem sucedida com status 200 ok
     if (response.statusCode == 200) {
+      // se sim retorna o corpo da requisição em formato Map<String, dynamic> 
       return json.decode(response.body);
     } else {
+      // Se não, retorna null e informa que houve erro ao buscar as informações
       print('Erro ao obter informações do usuário: ${response.statusCode}');
       return null;
     }
   }
 
+  //build da tela principal com um botão para logar e a logo do github
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -186,19 +216,26 @@ class _LoginPageState extends State<LoginPage> {
   }
 }
 
+// Widget da tela de boas vindas após o login com sucesso
 class WelcomePage extends StatelessWidget {
+  //user info retornado pela api do github
   final Map<String, dynamic> userInfo;
 
   const WelcomePage({super.key, required this.userInfo});
 
+  //Função para realizar o logout
   Future<void> _logout(BuildContext context) async {
+    // Busca o token no storage do dispositivo e o remove
     final prefs = await SharedPreferences.getInstance();
     await prefs.remove('github_token');
 
     // Limpa os cookies para forçar o logout
+    // E solicitar o login no github novamente
+    // após o logout
     final cookieManager = WebviewCookieManager();
     await cookieManager.clearCookies();
 
+    // Altera a tela atual para a tela de login
     Navigator.pushReplacement(
       context,
       MaterialPageRoute(
@@ -207,6 +244,7 @@ class WelcomePage extends StatelessWidget {
     );
   }
 
+  // widgets que mostram as informações do usuário logado
   @override
   Widget build(BuildContext context) {
     final String username = userInfo['login'] ?? 'Usuário';
